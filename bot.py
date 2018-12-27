ee8edfac0659be179f2d268f74d04bef6a59daca
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-27 10:12:54 -0500

    Implement barebones tweepy bot

---
+++ bot.py
@@ -0,0 +1,10 @@
+import tweepy
+from secrets import consumer_key, consumer_secret, access_token, \
+    access_token_secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+print(user.name)
