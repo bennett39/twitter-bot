3d1f4a604a4d5643bb77e3a4937c9bd8a4faf0df
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-28 14:58:45 -0500

    Add loop that prints public tweets on timeline

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
+PYTHONIOENCODING='utf8'
+
+import sys
 import tweepy
 from secrets import consumer_key, consumer_secret, access_token, \
     access_token_secret
+
+print(sys.stdout.encoding)
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -8,3 +13,7 @@ api = tweepy.API(auth)
 
 user = api.me()
 print(user.name)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
