98076dd6bc0da31cd80393a86da2135b9cd81467
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-29 11:12:48 -0500

    Implement multiple urls list for favoriting

--- bot.py
+++ bot.py
@@ -7,13 +7,16 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-search = "url:ed8d13397c9c"
-numberOfTweets = 100
-for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
-    try:
-        tweet.favorite()
-        print('Favorited a tweet')
-    except tweepy.TweepError as e:
-        print(e.reason)
-    except StopIteration:
-        break
+urls = ["url:ed8d13397c9c", "url:443940b8ef9f"]
+
+for url in urls:
+    search = url
+    numberOfTweets = 100
+    for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+        try:
+            tweet.favorite()
+            print('Favorited a tweet')
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
