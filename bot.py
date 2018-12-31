f35efd4c2f5f22335d3ae053d23ee1c0673ee79c
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-31 09:25:54 -0500

    Add comments

--- bot.py
+++ bot.py
@@ -1,3 +1,7 @@
+# A program to favorite tweets that mention certain URLs
+# Tutorial: https://bit.ly/2s2dtvS
+# Tweepy docs: http://docs.tweepy.org/en/3.7.0/
+
 import sys
 import tweepy
 from secrets import consumer_key, consumer_secret, access_token, \
@@ -7,6 +11,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+# Change these Twitter URL ids to whatever you want to search for
 urls = ["url:ed8d13397c9c", "url:443940b8ef9f"]
 
 for url in urls:
